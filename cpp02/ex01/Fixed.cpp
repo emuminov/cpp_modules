4e954125dd6236a8025164662c20b9085422a4c6
pdlmn <jseldardw@gmail.com>
2024-05-30 10:30:46 +0200

    feat: added the base for ex01

---
+++ cpp02/ex01/Fixed.cpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/28 16:39:11 by emuminov          #+#    #+#             */
+/*   Updated: 2024/05/30 02:14:50 by emuminov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+
+Fixed::Fixed(void) : _raw(0) {
+	std::cout << "Default constructor called\n";
+	return; }
+
+Fixed::Fixed(const int nbr) : _raw(nbr) {
+	std::cout << "Int constructor called\n";
+	return;
+}
+
+Fixed::Fixed(const float nbr) : _raw(nbr) {
+	std::cout << "Float constructor called\n";
+	return;
+}
+
+Fixed::~Fixed(void) { return; }
+
+Fixed::Fixed(const Fixed &f) {
+	std::cout << "Copy constructor called\n";
+	*this = f;
+	return;
+}
+
+Fixed & Fixed::operator=(const Fixed& f) {
+	std::cout << "Copy assignment operator called\n";
+	this->_raw = f.getRawBits();
+	return (*this);
+}
+
+int Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called\n";
+	return this->_raw;
+}
+
+void Fixed::setRawBits(int raw) {
+	this->_raw = raw;
+}
