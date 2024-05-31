134d777b9a6c4128e5ce927ec906dc5944f89b3e
Eldar Muminov <emuminov@z4r6p1.42lyon.fr>
2024-05-31 20:11:31 +0200

    style: changed variables names and the use of this

---
+++ cpp03/ex01/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/31 14:30:16 by emuminov          #+#    #+#             */
+/*   Updated: 2024/05/31 17:07:04 by emuminov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+
+int main (void) {
+	ClapTrap ct("George");
+	ScavTrap st("Leonard");
+	std::string target = "training dummy";
+	st.attack(target);
+	st.attack(target);
+	st.attack(target);
+	st.attack(target);
+	st.takeDamage(8);
+	st.beRepaired(3);
+	st.beRepaired(15);
+	st.attack(target);
+	st.attack(target);
+	st.attack(target);
+	st.attack(target);
+	st.attack(target);
+	st.attack(target);
+	st.takeDamage(17);
+	st.attack(target);
+	return 0;
+}
