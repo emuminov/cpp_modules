3f2c20f1447fd54d0fc598647977f2229a0ee7ef
pdlmn <jseldardw@gmail.com>
2024-06-01 22:19:53 +0200

    feat: added copy constructor and assignment operator overload to ClapTrap class

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -6,29 +6,31 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 14:30:16 by emuminov          #+#    #+#             */
-/*   Updated: 2024/05/31 16:33:00 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/06/01 22:18:56 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
 int main (void) {
-	ClapTrap ct("George");
+	ClapTrap ct1("George");
+	ClapTrap ct2(ct1);
+	ClapTrap ct3 = ct1;
 	std::string target = "training dummy";
-	ct.attack(target);
-	ct.attack(target);
-	ct.attack(target);
-	ct.attack(target);
-	ct.takeDamage(8);
-	ct.beRepaired(3);
-	ct.beRepaired(15);
-	ct.attack(target);
-	ct.attack(target);
-	ct.attack(target);
-	ct.attack(target);
-	ct.attack(target);
-	ct.attack(target);
-	ct.takeDamage(17);
-	ct.attack(target);
+	ct1.attack(target);
+	ct1.attack(target);
+	ct1.attack(target);
+	ct1.attack(target);
+	ct1.takeDamage(8);
+	ct1.beRepaired(3);
+	ct1.beRepaired(15);
+	ct1.attack(target);
+	ct1.attack(target);
+	ct1.attack(target);
+	ct1.attack(target);
+	ct1.attack(target);
+	ct1.attack(target);
+	ct1.takeDamage(17);
+	ct1.attack(target);
 	return 0;
 }
