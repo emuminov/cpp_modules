5c6c92174bc325e8deef145ab3e88573484b4b14
Eldar Muminov <emuminov@z2r1p7.42lyon.fr>
2024-07-17 16:16:12 +0200

    style: renamed some of the variables

--- cpp01/ex03/HumanA.cpp
+++ cpp01/ex03/HumanA.cpp
@@ -6,20 +6,20 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 17:09:52 by emuminov          #+#    #+#             */
-/*   Updated: 2024/05/18 17:35:07 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/07/17 16:15:14 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanA.hpp"
 #include <iostream>
 
-HumanA::HumanA(std::string name, Weapon& weapon) : name(name), weapon(weapon) {
+HumanA::HumanA(std::string name, Weapon& w) : name(name), weapon(w) {
     return;
 }
 
 HumanA::~HumanA() { return; }
 
 void HumanA::attack() const {
-    std::cout << this->name << " attacks with their " << this->weapon.getType()
+    std::cout << name << " attacks with their " << weapon.getType()
               << '\n';
 }
