86851a479efec0699bc4c99d7b5284badf67623c
Eldar Muminov <emuminov@z2r1p5.42lyon.fr>
2024-07-18 20:30:58 +0200

    fix: removed most of the this->, fixed fix point multiplication and deletion, and guard for copy assign operator

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 16:39:18 by emuminov          #+#    #+#             */
-/*   Updated: 2024/06/11 22:04:09 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/07/18 18:53:16 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,22 @@
 int main(void) {
     Fixed a;
     Fixed const b(Fixed(5.05f) * Fixed(2));
-    std::cout << a << std::endl;
-    std::cout << ++a << std::endl;
-    std::cout << a << std::endl;
-    std::cout << a++ << std::endl;
-    std::cout << a << std::endl;
-    std::cout << b << std::endl;
-    std::cout << Fixed::max(a, b) << std::endl;
+    Fixed const c(Fixed(10.10f) / Fixed(2));
+    Fixed const d(Fixed(10.10f) + Fixed(2));
+    Fixed const f(Fixed(10.10f) - Fixed(2));
+    std::cout << a << "\n";
+    std::cout << ++a << "\n";
+    std::cout << a << "\n";
+    std::cout << a++ << "\n";
+    std::cout << a << "\n";
+    std::cout << b << "\n";
+    std::cout << c << "\n";
+    std::cout << d << "\n";
+    std::cout << f << "\n";
+	std::cout << (a <= b) << "\n";
+	std::cout << (c >= d) << "\n";
+	std::cout << (f > d) << "\n";
+	std::cout << (f < c) << "\n";
+    std::cout << Fixed::max(a, b) << "\n";
     return 0;
 }
