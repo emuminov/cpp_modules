b8b5643b185cea48555751d6fc80c78678c8a452
Eldar Muminov <emuminov@z3r5p1.42lyon.fr>
2024-07-23 18:49:16 +0200

    test: added better tests for the ex00

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 14:30:16 by emuminov          #+#    #+#             */
-/*   Updated: 2024/06/01 22:18:56 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/07/23 18:48:50 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,23 @@
 int main(void) {
     ClapTrap ct1("George");
     ClapTrap ct2(ct1);
-    ClapTrap ct3 = ct1;
+    ClapTrap ct3("Don");
+	ct3 = ct1;
     std::string target = "training dummy";
-    ct1.attack(target);
-    ct1.attack(target);
-    ct1.attack(target);
-    ct1.attack(target);
-    ct1.takeDamage(8);
-    ct1.beRepaired(3);
-    ct1.beRepaired(15);
-    ct1.attack(target);
-    ct1.attack(target);
-    ct1.attack(target);
-    ct1.attack(target);
-    ct1.attack(target);
-    ct1.attack(target);
-    ct1.takeDamage(17);
-    ct1.attack(target);
+    ct3.attack(target);
+    ct3.attack(target);
+    ct3.attack(target);
+    ct3.attack(target);
+    ct3.takeDamage(8);
+    ct3.beRepaired(3);
+    ct3.beRepaired(15);
+    ct3.attack(target);
+    ct3.attack(target);
+    ct3.attack(target);
+    ct3.attack(target);
+    ct3.attack(target);
+    ct3.attack(target);
+    ct3.takeDamage(17);
+    ct3.attack(target);
     return 0;
 }
