a05f097ce6688bfdf2e16a5b3e40853c7ec6fbf2
Eldar Muminov <emuminov@z3r3p1.42lyon.fr>
2024-07-23 18:36:46 +0200

    feat: added ex03 solution

---
+++ cpp03/ex03/FragTrap.cpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/31 16:38:04 by emuminov          #+#    #+#             */
+/*   Updated: 2024/07/22 19:24:36 by emuminov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "FragTrap.hpp"
+#include <iostream>
+#include <string>
+
+FragTrap::FragTrap(std::string name) : ClapTrap(name, default_hit_points, default_energy_points, default_attack_damage) {
+    std::cout << "A wild FragTrap appears!\n";
+}
+
+FragTrap::~FragTrap() {
+    std::cout << "FragTrap suddenly disappears...\n";
+    return;
+}
+
+FragTrap::FragTrap(const FragTrap& ct) : ClapTrap(ct.m_name, default_hit_points, default_energy_points, default_attack_damage) {
+    std::cout << "FragTrap " << m_name << " clones itself!\n";
+    return;
+}
+
+FragTrap& FragTrap::operator=(const FragTrap& ct) {
+    m_name = ct.m_name;
+    std::cout << "FragTrap " << m_name << " clones itself!\n";
+    return *this;
+}
+
+void FragTrap::highFiveGuys() {
+	if (m_hit_points <= 0) {
+        std::cout << "FragTrap is dead!..\n";
+        return;
+    } else if (m_energy_points <= 0) {
+        std::cout << "Not enough energy...\n";
+        return;
+    }
+    m_energy_points--;
+    std::cout << "FragTrap " << m_name << " gives high five!";
+}
+
+void FragTrap::attack(const std::string& target) {
+    if (m_hit_points <= 0) {
+        std::cout << "FragTrap is dead!..\n";
+        return;
+    } else if (m_energy_points <= 0) {
+        std::cout << "Not enough energy...\n";
+        return;
+    }
+    m_energy_points--;
+    std::cout << "FragTrap " << m_name << " attacks " << target << ", causing "
+              << m_attack_damage << " points of damage!\n";
+}
