928652e31004de54ff4d327021383aa818f316dd
Eldar Muminov <emuminov@z3r5p1.42lyon.fr>
2024-07-23 18:58:04 +0200

    t

--- cpp03/ex02/main.cpp
+++ cpp03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 14:30:16 by emuminov          #+#    #+#             */
-/*   Updated: 2024/06/03 13:44:13 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/07/23 18:56:41 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 int main(void) {
     FragTrap ft1("Leonard");
     FragTrap ft2(ft1);
-    FragTrap ft3 = ft1;
+    FragTrap ft3("Don");
+	ft3 = ft1;
     std::string target = "training dummy";
     ft3.attack(target);
     ft3.attack(target);
@@ -31,7 +32,10 @@ int main(void) {
     ft3.attack(target);
     ft3.attack(target);
     ft3.attack(target);
-    ft3.takeDamage(17);
+    ft3.takeDamage(90);
+	ft3.highFiveGuys();
+    ft3.attack(target);
+    ft3.takeDamage(90);
     ft3.attack(target);
     return 0;
 }
