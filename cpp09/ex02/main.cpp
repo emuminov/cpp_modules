55278e1284470f2c8cf28c3f057610571275676d
Eldar Muminov <emuminov@z2r4p8.42lyon.fr>
2024-10-26 17:11:57 +0200

    feat: implemented mostly correct jacobsthal sequence insertion

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -27,72 +27,52 @@
 //     b1                                a1
 // ((((6 15) (8 16)) ((2 11) (0 17))) (((1 8) (3 10)) ((9 18) (14 19)))) | 5 12 4 20 7 13
 
+// because there is only one pair, I'll do it in one step
 // === 4 === insert at the start of S the smallest number of the smallest pair (b1)
-// 
+// === 4 === insert the rest of a's into the S
+// === 4 === insert b's into S using jacobsthal numbers and binary search
+// 6 15 8 16 2 11 0 17 1 8 3 10 9 18 14 19 5 12 4 20 7 13
 
-
-// 2 0 3 8 1 9 5 4 7
-// === 1 === swap pairs of numbers
-// (2 0) (3 8) (1 9) (5 4) 7
-// b1 a1 b2 a2 b3 a3 b4 a4
-// (0 2) (3 8) (1 9) (4 5) 7
-
-// === 2 === swap pairs of pairs of numbers
-// ((0 2) (3 8)) ((1 9) (4 5)) 7
-//   b1    a1      b2    a2
-// ((0 2) (3 8)) ((4 5) (1 9)) 7
-
-// === 3 === swap pairs of pairs of pairs of numbers
-// (((0 2) (3 8)) ((4 5) (1 9))) 7
-//       b1            a1
-// (((0 2) (3 8)) ((4 5) (1 9))) 7
-// pair_level = 4
-// size = 8 / 4
-// we want to compare 3 and 7
-// 8 9
-
-// === 4 === swap pairs of pairs of pairs of pairs of numbers
-// ((((0 2) (3 8) (4 5) (1 9)))) 7
-// pair_level = 8
-// size = 8 / 8
-// we want to compare 7
-// 9
-// RETURN
-
+// 6 15 8 16 2 11 0 17 1 8 3 10 9 18 14 19 5 12 4 20 7 13
 // === 3 === insert at the start of S the smallest number of the smallest pair (b1)
-// (((0 2) (3 8)) ((4 5) (1 9))) 7
-//       b1            a1
-// (((0 2) (3 8)) ((4 5) (1 9))) 7
-// main: [0 2 3 8 4 5 1 9]
+// === 3 === insert the rest of a's into the S
+// (((6 15) (8 16)) ((2 11) (0 17))) (((1 8) (3 10)) ((9 18) (14 19))) | (((5 12) (4 20))) 7 13
+//    b1              a1                b2             a2                   b3
+// pend: [10]    [20]
+//        b2      b3
+//        b1  a1  a2
+// main: [16, 17, 19]
 
-// === 3 === insert the remaing pend numbers to S
-// (((0 2) (3 8)) ((4 5) (1 9))) 7
-//       b1            a1
-// (((0 2) (3 8)) ((4 5) (1 9))) 7
-// main: [0 2 3 8 4 5 1 9]
+// === 3 === insert b's into S using jacobsthal numbers and binary search
+//        b2  b1  a1  a2  b3
+// main: [10, 16, 17, 19, 20]
+//    b2              b1                a1             a2                   b3
+// (((1 8) (3 10)) ((6 15) (8 16))) (((2 11) (0 17)) ((9 18) (14 19))) (((5 12) (4 20))) 7 13
 
+// 1 8 3 10 6 15 8 16 2 11 0 17 9 18 14 19 5 12 4 20 7 13
 // === 2 === insert at the start of S the smallest number of the smallest pair (b1)
-// ((0 2) (3 8)) ((4 5) (1 9)) 7
-//   b1    a1      b2    a2
-// ((0 2) (3 8)) ((4 5) (1 9)) 7
-// [0 2 3 8 1 9]
+// === 2 === insert the rest of a's into the S
+//   b1     a1       b2     a2       b3     a3      b4      a4        b5     a5        b6
+// ((1 8)  (3 10)) ((6 15) (8 16)) ((2 11) (0 17)) ((9 18) (14 19)) ((5 12) (4 20)) | (7 13)
+// pend: [15, 11, 18, 12]    [13]
+//        b2  b3  b4  b5      b6
+//        b1  a1  a2  a3  a4  a5
+// main: [ 8, 10, 16, 17, 19, 20]
 
-// === 2 === insert the remaing pend numbers to S
-// ((0 2) (3 8)) ((4 5) (1 9)) 7
-//   b1    a1      b2    a2
-// ((0 2) (4 5)) ((3 8) (1 9)) 7
-// [0 2 4 5 3 8 1 9]
+// === 2 === insert b's into S using jacobsthal numbers and binary search
+//        b1  a1  b3  b5  b6  b2, a2  a3  b4  a4  a5
+// main: [ 8, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20]
+// 1 8 3 10 2 11 5 12 7 13 6 15 8 16 0 17 9 18 14 19 4 20
 
+// 1 8 3 10 2 11 5 12 7 13 6 15 8 16 0 17 9 18 14 19 4 20
 // === 1 === insert at the start of S the smallest number of the smallest pair (b1)
-// (0 2) (4 5) (3 8) (1 9) 7
-// b1 a1 b2 a2 b3 a3 b4 a4
-//
-// [0 2 5 8 9]
-
-// === 1 === insert the remaing pend numbers to S
-// (0 2) (4 5) (3 8) (1 9) 7
-// b1 a1 b2 a2 b3 a3 b4 a4
-// [0 1 2 3 4 5 7 8 9]
+// === 1 === insert the rest of a's into the S
+// (1 8) (3 10) (2 11) (5 12) (7 13) (6 15) (8 16) (0 17) (9 18) (14 19) (4  20)
+// b1 a1 b2 a2  b3 a3  b4 a4  b5 a5  b6 a6  b7 a7  b8 a8  b9 a9  b10 a10 b11 a11
+// pend: [3  2  5  7  6  8  0  9  14  4]
+//        b2 b3 b4 b5 b6 b7 b8 b9 b10 b11
+//        b1 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11
+// main: [1  8  10 11 12 13 15 16 17 18 19  20]
 
 int main(void) {
 	// int arr[] = {11, 2, 17, 0, 16, 8, 6, 15, 10, 3, 8, 1, 18, 9, 14, 19, 12, 5, 4, 20, 13, 7};
