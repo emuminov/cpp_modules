dcf185243e9c56131b7233f645f829119f6da55a
emuminov <jseldardw@gmail.com>
2025-02-16 17:38:44 +0100

    fix: fixed the error on getting the error on empty argument to the program

--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -6,16 +6,20 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 19:19:17 by emuminov          #+#    #+#             */
-/*   Updated: 2024/05/20 19:52:58 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/07/17 16:21:52 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 #include <iostream>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]) {
     if (argc != 2)
-        std::cerr << "*mumble mumble*";
+	{
+        std::cerr << "*mumble mumble*\n";
+		return EXIT_FAILURE;
+	}
     Harl h = Harl();
     h.complain(argv[1]);
     return 0;
