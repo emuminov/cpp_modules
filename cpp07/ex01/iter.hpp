68b672f1f0192a6778d23e1a17c6ac62af6713f5
emuminov <jseldardw@gmail.com>
2025-02-16 17:38:45 +0100

    style: applied clang-format to the code

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -1,46 +1,39 @@
 #ifndef ITER_HPP
 #define ITER_HPP
 #include <iostream>
-template<typename T>
-void iter(T* arr, const int len, void (fn)(T&, int)) {
-	if (!arr)
-		return;
-	for (int i = 0; i < len; i++) {
-		fn(arr[i], i);
-	}
+template <typename T> void iter(T* arr, const int len, void(fn)(T&, int)) {
+    if (!arr)
+        return;
+    for (int i = 0; i < len; i++) {
+        fn(arr[i], i);
+    }
 }
 
-template<typename T>
-void iter(T* arr, const int len, void (fn)(T*)) {
-	if (!arr)
-		return;
-	for (int i = 0; i < len; i++) {
-		fn(&arr[i]);
-	}
+template <typename T> void iter(T* arr, const int len, void(fn)(T*)) {
+    if (!arr)
+        return;
+    for (int i = 0; i < len; i++) {
+        fn(&arr[i]);
+    }
 }
 
-template<typename T>
-void iter(T* arr, const int len, void (fn)(T&)) {
-	if (!arr)
-		return;
-	for (int i = 0; i < len; i++) {
-		fn(arr[i]);
-	}
+template <typename T> void iter(T* arr, const int len, void(fn)(T&)) {
+    if (!arr)
+        return;
+    for (int i = 0; i < len; i++) {
+        fn(arr[i]);
+    }
 }
 
-template<typename T>
-void print_arr(T* arr, const  int len) {
-	std::cout << "[";
-	for (int i = 0; i < len; i++) {
-		std::cout << arr[i];
-		if (i != (len - 1))
-			std::cout << ", ";
-	}
-	std::cout << "]\n";
+template <typename T> void print_arr(T* arr, const int len) {
+    std::cout << "[";
+    for (int i = 0; i < len; i++) {
+        std::cout << arr[i];
+        if (i != (len - 1))
+            std::cout << ", ";
+    }
+    std::cout << "]\n";
 }
 
-template<typename T>
-void inc(T& incrementable) {
-		incrementable++;
-}
+template <typename T> void inc(T& incrementable) { incrementable++; }
 #endif
