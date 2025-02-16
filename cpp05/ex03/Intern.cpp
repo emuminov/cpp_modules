ab8fcf3d9dc3fdece82d078df59d6abae98a8985
emuminov <jseldardw@gmail.com>
2025-02-16 17:38:44 +0100

    refactor: changed implementation of Intern::makeForm in cpp05-ex03 to use an array of function pointers

--- cpp05/ex03/Intern.cpp
+++ cpp05/ex03/Intern.cpp
@@ -11,25 +11,36 @@ Intern& Intern::operator=(const Intern& i) { (void)i; return *this; }
 
 Intern::~Intern() { return; }
 
-AForm* Intern::makeForm(const std::string& form_name, const std::string& target) const {
-	const char* form_names[] = {
-		"robotomy request",
-		"presidential pardon",
-		"shrubbery creation"
+AForm* Intern::make_robotomy_request_form(const std::string& target) const {
+	return new RobotomyRequestForm(target);
+}
+
+AForm* Intern::make_presidential_pardon_form(const std::string& target) const {
+	return new PresidentialPardonForm(target);
+}
+
+AForm* Intern::make_shrubbery_creation_form(const std::string& target) const {
+	return new ShrubberyCreationForm(target);
+}
+
+AForm* Intern::makeForm(const std::string& target_form_name, const std::string& target) const {
+	const t_form_creator form_creators[] = {
+		{ "robotomy request", &Intern::make_robotomy_request_form },
+		{ "presidential pardon", &Intern::make_presidential_pardon_form },
+		{ "shrubbery creation", &Intern::make_shrubbery_creation_form }
 	};
+
 	int i = 0;
-	int l = ((int)sizeof(form_names) / (int)sizeof(char*));
-	while ((i < l && (form_names[i] != form_name))) { i++; }
+	int l = ((int)sizeof(form_creators) / (int)sizeof(t_form_creator));
+	while ((i < l && (form_creators[i].name != target_form_name))) { i++; }
+
 	switch (i) {
 		case 0:
-			std::cout << "Intern creates " << form_name << "form\n";
-			return new RobotomyRequestForm(target); break;
 		case 1:
-			std::cout << "Intern creates " << form_name << "form\n";
-			return new PresidentialPardonForm(target); break;
 		case 2:
-			std::cout << "Intern creates " << form_name << "form\n";
-			return new ShrubberyCreationForm(target); break;
+			std::cout << "Intern creates " << target_form_name << "form\n";
+			return (this->*form_creators[i].create_form)(target);
+
 		default:
 			throw InvalidFormNameException();
 	}
