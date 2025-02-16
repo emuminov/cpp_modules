6ac62a9a44830b6c4784135ce3fb09109c35c353
emuminov <jseldardw@gmail.com>
2025-02-16 17:38:44 +0100

    git: updated .gitignore feat: updated main to accept arguments

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -1,29 +1,11 @@
 #include "ScalarConverter.hpp"
 #include <iostream>
+#include <string>
 
-void test_converter(std::string str) {
-	std::cout << "Tested literal: " << str << "\n";
-	ScalarConverter::convert(str);
-	std::cout << "===\n";
-}
-
-int main (void) {
-	test_converter("42");
-	test_converter("0");
-	test_converter("2147483647");
-	test_converter("2147483648");
-	test_converter("-2147483648");
-	test_converter("-2147483649");
-	test_converter("a");
-	test_converter("ab");
-	test_converter("5.24f");
-	test_converter("42.84f");
-	test_converter("42.84");
-	test_converter("nan");
-	test_converter("nanf");
-	test_converter("inf");
-	test_converter("inff");
-	test_converter("-inf");
-	test_converter("-inff");
+int main(int argc, char** argv) {
+	if (argc != 2)
+		std::cout << "Correct usage: ./convert [scalar literal]";
+	const std::string arg = argv[1];
+	ScalarConverter::convert(arg);
 	return 0;
 }
