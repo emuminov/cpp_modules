4306de0597d87ae9b7ac48c508e5d7bf9e9c0258
emuminov <jseldardw@gmail.com>
2025-02-16 17:38:45 +0100

    style: applied clang-format to the cpp08

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -2,15 +2,15 @@
 #include <iostream>
 
 int main(void) {
-	Span sp(8);
-	sp.addNumber(6);
-	sp.addNumber(3);
-	sp.addNumber(11);
-	sp.addNumber(1);
-	sp.addNumber(21);
-	sp.addNumber(17);
-	sp.addNumber(9);
-	std::cout << sp << "\n";
-	std::cout << sp.shortestSpan() << std::endl;
-	std::cout << sp.longestSpan() << std::endl;
+    Span sp(8);
+    sp.addNumber(6);
+    sp.addNumber(3);
+    sp.addNumber(11);
+    sp.addNumber(1);
+    sp.addNumber(21);
+    sp.addNumber(17);
+    sp.addNumber(9);
+    std::cout << sp << "\n";
+    std::cout << sp.shortestSpan() << std::endl;
+    std::cout << sp.longestSpan() << std::endl;
 }
