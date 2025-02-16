f6cc95cf8a65ef57ac8240c1ace07589ad5f76bc
emuminov <jseldardw@gmail.com>
2025-02-16 17:38:44 +0100

    fix: added the return statement in main after the error

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -3,8 +3,10 @@
 #include <string>
 
 int main(int argc, char** argv) {
-	if (argc != 2)
+	if (argc != 2) {
 		std::cout << "Correct usage: ./convert [scalar literal]";
+		return 1;
+	}
 	const std::string arg = argv[1];
 	ScalarConverter::convert(arg);
 	return 0;
