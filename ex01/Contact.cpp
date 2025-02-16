9448375381f9a19dcaa1f82de25f1867db70327a
emuminov <jseldardw@gmail.com>
2025-02-16 17:38:43 +0100

    feat: added exiting with message, proper searching, borders for the table, splitted the code into couple of files

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -5,54 +5,55 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/04 15:43:03 by emuminov          #+#    #+#             */
-/*   Updated: 2024/05/07 18:53:08 by emuminov         ###   ########.fr       */
+/*   Created: 2024/05/04 16:15:57 by emuminov          #+#    #+#             */
+/*   Updated: 2024/05/10 16:17:04 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Contact.hpp"
+
 #include "iostream"
 
 Contact::Contact()
     : first_name(""), last_name(""), nickname(""), phone_number(""),
       darkest_secret("") {
-  return;
+    return;
 }
 
 Contact::~Contact() { return; }
 
 bool Contact::set_first_name(std::string first_name) {
-  if (first_name == "")
-    return false;
-  this->first_name = first_name;
-  return true;
+    if (first_name == "")
+        return false;
+    this->first_name = first_name;
+    return true;
 }
 
 bool Contact::set_last_name(std::string last_name) {
-  if (last_name == "")
-    return false;
-  this->last_name = last_name;
-  return true;
+    if (last_name == "")
+        return false;
+    this->last_name = last_name;
+    return true;
 }
 
 bool Contact::set_nickname(std::string nickname) {
-  if (nickname == "")
-    return false;
-  this->nickname = nickname;
-  return true;
+    if (nickname == "")
+        return false;
+    this->nickname = nickname;
+    return true;
 }
 bool Contact::set_phone_number(std::string phone_number) {
-  if (phone_number == "")
-    return false;
-  this->phone_number = phone_number;
-  return true;
+    if (phone_number == "")
+        return false;
+    this->phone_number = phone_number;
+    return true;
 }
 
 bool Contact::set_darkest_secret(std::string darkest_secret) {
-  if (darkest_secret == "")
-    return false;
-  this->darkest_secret = darkest_secret;
-  return true;
+    if (darkest_secret == "")
+        return false;
+    this->darkest_secret = darkest_secret;
+    return true;
 }
 
 std::string Contact::get_first_name() { return this->first_name; }
--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/03 19:46:38 by emuminov          #+#    #+#             */
-/*   Updated: 2024/05/08 14:42:54 by emuminov         ###   ########.fr       */
+/*   Created: 2024/05/03 16:19:13 by emuminov          #+#    #+#             */
+/*   Updated: 2024/05/10 16:19:26 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,20 @@ PhoneBook::PhoneBook() : _contacts_pointer(0), _len(0) { return; }
 PhoneBook::~PhoneBook() { return; }
 
 void PhoneBook::add_contact(Contact contact) {
-	this->_contacts[this->_contacts_pointer] = contact;
-	this->_contacts_pointer++;
-	if (this->_contacts_pointer >= 8)
-		this->_contacts_pointer = 0;
-	if (this->_len < 8)
-		this->_len++;
-	return;
+    this->_contacts[this->_contacts_pointer] = contact;
+    this->_contacts_pointer++;
+    if (this->_contacts_pointer >= 8)
+        this->_contacts_pointer = 0;
+    if (this->_len < 8)
+        this->_len++;
+    return;
 }
 
 Contact PhoneBook::get_contact(unsigned int index) {
-	if (index > this->_len - 1) {
-		return this->_contacts[this->_len - 1];
-	}
-	return this->_contacts[index];
+    if (index > this->_len - 1) {
+        return this->_contacts[this->_len - 1];
+    }
+    return this->_contacts[index];
 }
 
-unsigned int PhoneBook::get_len() {
-	return this->_len;
-}
+unsigned int PhoneBook::get_len() { return this->_len; }
