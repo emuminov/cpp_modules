39734106980edfe379174e4742c2e18dd649cdcf
emuminov <jseldardw@gmail.com>
2025-02-16 17:38:44 +0100

    test: added more tests for the Form

--- cpp05/ex01/main.cpp
+++ cpp05/ex01/main.cpp
@@ -16,5 +16,31 @@ int main (void) {
 	std::cout << f2 << '\n';
 	b2.signForm(f2);
 	std::cout << f2 << '\n';
+
+	std::cout << "===\n";
+
+	// will throw
+	try {
+		Form f3("billing form", 0, 10);
+		std::cout << f3 << '\n';
+		b1.signForm(f3);
+		std::cout << f3 << '\n';
+	}
+	catch (std::exception& e) {
+		std::cerr << e.what() << '\n';
+	}
+
+	std::cout << "===\n";
+
+	// will throw
+	try {
+		Form f3("billing form", 1, 151);
+		std::cout << f3 << '\n';
+		b1.signForm(f3);
+		std::cout << f3 << '\n';
+	}
+	catch (std::exception& e) {
+		std::cerr << e.what() << '\n';
+	}
 	return 0;
 }
